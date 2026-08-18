daf7e4cf1c7d88ae807649cf0c1a4e6954198812
Nandu Kumar <mnandu2312@gmail.com>
2026-08-18 11:49:55 +0530

    Fix role authorization decorator

--- utils/decoraters.py
+++ utils/decoraters.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 from models.user import User
 
 
-def role_required(allowed_roles):
+def role_required(*allowed_roles):
 
     def decorator(fn):
 
